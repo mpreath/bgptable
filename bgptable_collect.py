dd4b510e96798528628cbb0ebba13b03a0073fce
Matt Reath <mpreath@gmail.com>
2015-06-26 17:57:28 -0500

    Fixed broken logic in building graphs. Assumed Sun was day 1 for the .isocalendar() function but Mon is.

--- bgptable_collect.py
+++ bgptable_collect.py
@@ -4,12 +4,12 @@
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # bgptables is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with onstools.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -17,6 +17,7 @@ import tweepy
 import re
 import sqlite3
 from dateutil import parser
+import ConfigParser
 
 con_key = "BBJ4wtXpErzYTkZ5K28yc4Ype"
 con_secret = "okNbaGjgjAg0txCgTPXbtLwLYNbs3RI2Q6sYqio6JAZNwNpxPW"
@@ -41,19 +42,18 @@ curs.execute('SELECT name FROM sqlite_master WHERE type=\'table\' AND name=\'bgp
 rows = curs.fetchall()
 if not rows:
     curs.execute('''CREATE TABLE bgp4_statuses(status_id INT PRIMARY KEY, prefixes INT, status_date_time TEXT)''')
-    
+
 curs.execute('SELECT * FROM bgp4_statuses ORDER BY status_id DESC LIMIT 1')
 rows = curs.fetchall()
 
 if rows:
     status_id = rows[0][0]
-
     # get all statuses newer than the most recent status_id in the db
     bgp4_statuses = api.user_timeline(screen_name='bgp4_table',since_id=status_id)
 else:
     # this should only run the very first time when there are no statuses in the db
     # we'll grab 5 and put them in the database
-    bgp4_statuses = api.user_timeline(screen_name='bgp4_table',count=5)
+    bgp4_statuses = api.user_timeline(screen_name='bgp4_table',count=30)
 
 
 for status in bgp4_statuses:
@@ -66,8 +66,8 @@ for status in bgp4_statuses:
         print(numbers[0].strip())
         ins = 'INSERT INTO bgp4_statuses (status_id, prefixes, status_date_time) VALUES (?,?,?)'
         curs.execute(ins, (status.id, numbers[0].strip(), str(dt)[:-9]))
-        
-# IPV6 PREFIXES #        
+
+# IPV6 PREFIXES #
 
 
 curs.execute('SELECT name FROM sqlite_master WHERE type=\'table\' AND name=\'bgp6_statuses\'')
@@ -104,5 +104,3 @@ for status in bgp6_statuses:
 conn.commit()
 curs.close()
 conn.close()
-
-
--- bgptable_graph.py
+++ bgptable_graph.py
@@ -4,12 +4,12 @@
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # bgptables is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with onstools.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -82,76 +82,75 @@ bgp4_xlabels = ['','','','','','','']
 
 if len(rows) > 1:
     # we have more than 2 records available
-    
 
-    
+
+
     for row in rows:
         # lets build a couple stats
         dt = parser.parse(row[2])
         year,weeknum,dow = dt.isocalendar()
         c_year,c_weeknum,c_dow = dt.today().isocalendar()
-        
-        
+
         # % growth over the last day
         if(dt.date() == dt.today().date()):
-            
+
             if(row[1] > day_max):
                 day_max = row[1]
-                
+
             if(row[1] < day_min or day_min == 0):
                 day_min = row[1]
-            
+
             day_total += row[1]
             day_count += 1
-            
+
             if(day_count == 1):
                 day_first_value = row[1]
-                
+
             day_last_value = row[1]
-            
+
         # % growth over the last week
         if(weeknum == c_weeknum):
             # append to this week's data list
             if(row[1] > week_max):
                 week_max = row[1]
-                
+
             if(row[1] < week_min or week_min == 0):
                 week_min = row[1]
-            
+
             week_total += row[1]
             week_count += 1
-            
+
             if(week_count == 1):
                 week_first_value = row[1]
-                
+
             week_last_value = row[1]
-            
+
             # set the day value to the last value of that day
-            bgp4_day_list[dow] = row[1]
-            bgp4_xlabels[dow] = str(dt.date())
-            if(row[1] > bgp4_day_max[dow]):
-                bgp4_day_max[dow] = row[1]
-                
-            if(row[1] < bgp4_day_min[dow] or bgp4_day_min[dow] == 0):
-                bgp4_day_min[dow] = row[1]
-   
+            bgp4_day_list[dow-1] = row[1]
+            bgp4_xlabels[dow-1] = str(dt.date())
+            if(row[1] > bgp4_day_max[dow-1]):
+                bgp4_day_max[dow-1] = row[1]
+
+            if(row[1] < bgp4_day_min[dow-1] or bgp4_day_min[dow-1] == 0):
+                bgp4_day_min[dow-1] = row[1]
+
         # % growth over the last month
         if(dt.month == dt.today().month):
-            
+
             if(row[1] > month_max):
                 month_max = row[1]
-                
+
             if(row[1] < month_min or month_min == 0):
                 month_min = row[1]
-            
+
             month_total += row[1]
             month_count += 1
-            
+
             if(month_count == 1):
                 month_first_value = row[1]
-                
+
             month_last_value = row[1]
-                
+
 else:
     # not enough information to run statistics on
     # should probably just exit here
@@ -166,7 +165,12 @@ week_growth_percent = ((week_last_value - week_first_value)/float(week_first_val
 
 month_average = month_total / month_count
 month_growth_percent = ((month_last_value - month_first_value)/float(month_first_value))*100
-    
+
+
+print(bgp4_day_list)
+print(bgp4_day_min)
+print(bgp4_day_max)
+print(bgp4_xlabels)
 # # create graph
 #
 plt.figure(1)
@@ -187,7 +191,7 @@ file = open('weekly_bgp4_graph.png', 'rb')
 data = file.read()
 file_name = os.path.realpath(file.name)
 status_str = 'Weekly BGP4 Graph [{:3.3f}% Growth]'.format(week_growth_percent)
-api.update_with_media(status=status_str,filename=file_name)
+# api.update_with_media(status=status_str,filename=file_name)
 
 
 # Grab all of the IPv4 information in ascending order
@@ -227,88 +231,94 @@ bgp6_xlabels = ['','','','','','','']
 
 if len(rows) > 1:
     # we have more than 2 records available
-    
 
-    
+
+
     for row in rows:
         # lets build a couple stats
         dt = parser.parse(row[2])
         year,weeknum,dow = dt.isocalendar()
         c_year,c_weeknum,c_dow = dt.today().isocalendar()
-        
-        
+
+
         # % growth over the last day
         if(dt.date() == dt.today().date()):
-            
+
             if(row[1] > day_max):
                 day_max = row[1]
-                
+
             if(row[1] < day_min or day_min == 0):
                 day_min = row[1]
-            
+
             day_total += row[1]
             day_count += 1
-            
+
             if(day_count == 1):
                 day_first_value = row[1]
-                
+
             day_last_value = row[1]
-            
+
         # % growth over the last week
         if(weeknum == c_weeknum):
             # append to this week's data list
             if(row[1] > week_max):
                 week_max = row[1]
-                
+
             if(row[1] < week_min or week_min == 0):
                 week_min = row[1]
-            
+
             week_total += row[1]
             week_count += 1
-            
+
             if(week_count == 1):
                 week_first_value = row[1]
-                
+
             week_last_value = row[1]
-            bgp6_day_list[dow] = row[1]
-            bgp6_xlabels[dow] = str(dt.date())
-            if(row[1] > bgp6_day_max[dow]):
-                bgp6_day_max[dow] = row[1]
-                
-            if(row[1] < bgp6_day_min[dow] or bgp6_day_min[dow] == 0):
-                bgp6_day_min[dow] = row[1]
-   
+            bgp6_day_list[dow-1] = row[1]
+            bgp6_xlabels[dow-1] = str(dt.date())
+            if(row[1] > bgp6_day_max[dow-1]):
+                bgp6_day_max[dow-1] = row[1]
+
+            if(row[1] < bgp6_day_min[dow-1] or bgp6_day_min[dow-1] == 0):
+                bgp6_day_min[dow-1] = row[1]
+
         # % growth over the last month
         if(dt.month == dt.today().month):
-            
+
             if(row[1] > month_max):
                 month_max = row[1]
-                
+
             if(row[1] < month_min or month_min == 0):
                 month_min = row[1]
-            
+
             month_total += row[1]
             month_count += 1
-            
+
             if(month_count == 1):
                 month_first_value = row[1]
-                
+
             month_last_value = row[1]
-            
+
 
     day_average = day_total / day_count
     day_growth_percent = ((day_last_value - day_first_value)/float(day_first_value))*100
 
     week_average = week_total / week_count
     week_growth_percent = ((week_last_value - week_first_value)/float(week_first_value))*100
-    
+
     month_average = month_total / month_count
     month_growth_percent = ((month_last_value - month_first_value)/float(month_first_value))*100
-    
+
 else:
     # not enough information to run statistics on
     # should probably just exit here
     exit()
+
+
+print(bgp6_day_list)
+print(bgp6_day_min)
+print(bgp6_day_max)
+print(bgp6_xlabels)
 
 # create graph
 
@@ -330,9 +340,7 @@ file = open('weekly_bgp6_graph.png', 'rb')
 data = file.read()
 file_name = os.path.realpath(file.name)
 status_str = 'Weekly BGP6 Graph [{:3.3f}% Growth]'.format(week_growth_percent)
-api.update_with_media(status=status_str,filename=file_name)
+# api.update_with_media(status=status_str,filename=file_name)
 
 curs.close()
 conn.close()
-
-
